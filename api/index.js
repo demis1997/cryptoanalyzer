f931789f3f9fa901e7801bded33085b29f83108b
demis1997 <demis.konstantinou16@gmail.com>
2026-05-06 15:04:02 +0300

    php fix

---
+++ api/index.js
@@ -0,0 +1,6 @@
+import app from "../server.js";
+
+// Vercel Node Serverless Function entrypoint.
+// Exporting the Express app lets Vercel handle req/res without app.listen().
+export default app;
+
